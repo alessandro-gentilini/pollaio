99bbc069b5d3ed32398d420b2c3db094746ee109
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2016-01-17 11:55:54 +0100

    Add version 24 July 2010.

--- pollaio.ino
+++ pollaio.ino
@@ -3,53 +3,8 @@
  Il Pollaio di Cleto
  
  Autore: Alessandro alessandro.gentilini@gmail.com
- Data  : 30 maggio 2010
- 
- Descrizione
- -----------
- 
- Il pulsante rosso e' indicato con MANUAL_BTN.
- 
- I due pulsanti neri sono indicati con OPEN_BTN e CLOSE_BTN.
- 
- Se MANUAL_BTN e' premuto e si preme contemporaneamente OPEN_BTN (rispettivamente CLOSE_BTN) 
- il motore si muove nella direzione OPEN_DIR (CLOSE_DIR) fintanto che OPEN_BTN (CLOSE_BTN)
- e' premuto.
- 
- Se MANUAL_BTN non e' premuto, la pressione di OPEN_BTN (rispettivamente CLOSE_BTN) muove il
- motore nella direzione OPEN_DIR (CLOSE_DIR) fino ad avere LIMIT_SWT REACHED
- se door era CLOSED (OPENED).
- 
- Se MANUAL_BTN non e' premuto, e OPEN_BTN e CLOSE_BTN sono non premuti, e door e' CLOSED 
- (rispettivamente OPENED) allora sunrise==true (sunset==true) muove il motore  
- nella direzione OPEN_DIR (CLOSE_DIR) fino ad avere LIMIT_SWT REACHED. 
- 
- 
- Note
- ----
- 
- Ho provato il programma alimentando Arduino con la porta USB (+5VDC) e fornendo 
- sui fili arancioni la +12VDC  di alimentazione del motore (i due fili esterni
- arancioni, attenzione alla polarita'). 
- I due fili arancioni interni vanno al motore.
- In questa situazione di doppia alimentazione affinche' tutto funzioni e' necessario
- collegare lo zero di riferimento della +5VDC allo zero di riferimento della +12VDC.
- Nella versione finale anche Arduino sara' alimentato a +12VDC.
- 
- Collegando Arduino con USB ad un computer che abbia installati i driver FTDI
- alla USB verra' associata una porta seriale COM, con hyperterminal si puo'
- vedere l'output prodotto da Arduino collegandosi a 9600bps 8-1 none, altrimenti
- scaricare l'IDE da www.arduino.cc
- 
- Provare a leggere lo stato di un pin dichiarato come output per vedere se
- si può evitare la variabile motor_is_moving.
- 
- Il riconoscimento di alba e tramonto non e' corretto in tutte le situazioni: 
- attualmente funziona solamente se la transizione buio/luce (luce/buio) e'
- "repentina".
- 
- L'inglese e' un po' infelice ;-)
- 
+ Data  : 24 luglio 2010
+
  */
 
 // tempi in ms
@@ -119,18 +74,6 @@ public:
       && manual_btn==rhs.manual_btn && limit_swt==rhs.limit_swt;
     return !same;
   }
-
-//  void toString() const
-//  {
-//    Serial.print("\n");
-//    Serial.print(door,DEC);
-//    Serial.print(period,DEC);
-//    Serial.print(open_btn,DEC);
-//    Serial.print(close_btn,DEC);
-//    Serial.print(manual_btn,DEC);
-//    Serial.print(limit_swt,DEC);
-//    Serial.print("\n");
-//  }
 };
 
 Status s,sp;
@@ -150,128 +93,19 @@ void setup()
   digitalWrite( DIRECTION, OPEN_DIR );
 
   s.door = unknown_status;
+
   s.period = unknown_period;
+  s.start_open = millis();
+  s.start_close = millis();
+  
   Serial.begin(9600);
+  Serial.println("");
+  Serial.println("*** PROGRAMMA AVVIATO ***");
+  Serial.println("");
 }
 
 
 
-void error( char* msg )
-{
-  Serial.print("ERRORE: ");
-  Serial.println(msg);
-}
-
-void print_status()
-{
-  static char e1[]="FINECORSA IMPEGNATO E MOTORE IN MOVIMENTO";
-  static char e2[]="FINECORSA LIBERO E MOTORE FERMO";
-  char * error_str = 0;
-
-  Serial.println("-----------------------------------------");
-  Serial.println("INGRESSI:");
-  Serial.print("LUCE=");
-  Serial.print(s.light);  
-  Serial.print(" ");
-  switch(s.period)  
-  {
-  case day: 
-    Serial.println("GIORNO");
-    break;
-  case night: 
-    Serial.println("NOTTE");
-    break;
-  default: 
-    error("PERIODO IGNOTO");
-    break;
-  }
-
-  if ( s.open_btn == PRESSED )
-  {
-    Serial.println("PULSANTE APRI PREMUTO");
-  }
-  else
-  {
-    Serial.println("PULSANTE APRI NON PREMUTO");    
-  }
-
-  if ( s.close_btn == PRESSED )
-  {
-    Serial.println("PULSANTE CHIUDI PREMUTO");
-  }
-  else
-  {
-    Serial.println("PULSANTE CHIUDI NON PREMUTO");    
-  }
-
-  if ( s.manual_btn == PRESSED )
-  {
-    Serial.println("PULSANTE MANUALE PREMUTO");
-  }
-  else
-  {
-    Serial.println("PULSANTE MANUALE NON PREMUTO");    
-  }  
-
-  if ( s.limit_swt == REACHED )  
-  {
-    if ( digitalRead( MOTOR ) == GO ) 
-    {
-      error_str = e1;
-    }
-    Serial.println("FINECORSA IMPEGNATO");
-  }  
-  else  
-  {
-    if ( digitalRead( MOTOR ) == STOP ) 
-    {
-      error_str = e2;
-    }    
-    Serial.println("FINECORSA LIBERO");
-  }
-
-  Serial.println("\nUSCITE:");
-  if ( digitalRead( MOTOR ) == GO )
-  {
-    Serial.println("MOTORE IN MOVIMENTO");
-  }
-  else
-  {
-    Serial.println("MOTORE FERMO");
-  }
-
-  if ( digitalRead( DIRECTION ) == OPEN_DIR )
-  {
-    Serial.println("DIREZIONE APERTURA");
-  }
-  else
-  {
-    Serial.println("DIREZIONE CHIUSURA");
-  }
-
-  switch(s.door)  
-  {
-  case opened:  
-    Serial.println("PORTA APERTA"); 
-    break;
-  case closed:  
-    Serial.println("PORTA CHIUSA"); 
-    break;
-  case unknown_status: 
-    Serial.println("PORTA ??????"); 
-    break;
-  default: 
-    error("STATO PORTA INASPETTATO"); 
-    break;
-  }
-
-  if ( error_str ) 
-  {
-    Serial.println("\nERRORI");
-    error( error_str );
-  }
-  Serial.println("-----------------------------------------");
-}
 
 void loop()
 {
@@ -312,8 +146,6 @@ void loop()
       if ( s.manual_btn != PRESSED && digitalRead( MOTOR ) == GO && (now - s.start_open) > MAX_OPEN_TIME )
       {
         digitalWrite( MOTOR, STOP );  
-        Serial.println(now);
-        Serial.println(s.start_open);
         error("L'APERTURA HA IMPIEGATO TROPPO TEMPO");
       }
     }
@@ -322,8 +154,6 @@ void loop()
       if ( s.manual_btn != PRESSED && digitalRead( MOTOR ) == GO && (now - s.start_close) > MAX_CLOSE_TIME )
       {
         digitalWrite( MOTOR, STOP );              
-        Serial.println(now);       
-        Serial.println(s.start_close);
         error("LA CHIUSURA HA IMPIEGATO TROPPO TEMPO");
       }      
     }
@@ -346,16 +176,25 @@ void loop()
     {
       digitalWrite( DIRECTION, OPEN_DIR );
       digitalWrite( MOTOR, GO );
+      
+      s.start_open = millis();
       s.door = unknown_status;
-      Serial.println("\nCOMANDO APERTURA ATTIVO\n");
+      
+      Serial.println("");
+      Serial.println("COMANDO APERTURA ATTIVO");
+      Serial.println("");      
     }
 
     if ( s.open_btn == RELEASED && s.close_btn == PRESSED )    
     {
       digitalWrite( DIRECTION, CLOSE_DIR );      
       digitalWrite( MOTOR, GO );
+      
+      s.start_close = millis();
       s.door = unknown_status;
-      Serial.println("\nCOMANDO CHIUSURA ATTIVO\n");
+      
+      Serial.println("");
+      Serial.println("COMANDO CHIUSURA ATTIVO");
     }
 
   }  
@@ -367,12 +206,13 @@ void loop()
       digitalWrite( MOTOR, GO );
       if ( s.door == closed ) 
       {
-        Serial.println("\nAPERTURA AUTOMATICA PORTA\n");
+        Serial.println("");
+        Serial.println("APERTURA AUTOMATICA PORTA");
+        Serial.println("");
         delay(INRUSH_TIME);// per dare tempo al finecorsa di disimpegnarsi
         s.start_open = millis();
       }      
       s.door = unknown_status;
-      Serial.println("\nAPERTURA AUTOMATICA PORTA IN CORSO\n");
     }
 
     if ( ( s.door == opened || s.door == unknown_status ) && s.period == night )    
@@ -381,12 +221,13 @@ void loop()
       digitalWrite( MOTOR, GO );
       if ( s.door == opened ) 
       {
-        Serial.println("\nCHIUSURA AUTOMATICA PORTA\n");        
+        Serial.println("");
+        Serial.println("CHIUSURA AUTOMATICA PORTA");        
+        Serial.println("");
         delay(INRUSH_TIME);// per dare tempo al finecorsa di disimpegnarsi
         s.start_close = millis();
       }      
       s.door = unknown_status;
-      Serial.println("\nCHIUSURA AUTOMATICA PORTA IN CORSO\n");
     }      
   }
 
@@ -394,22 +235,49 @@ void loop()
 }
 
 
+void error( char* msg )
+{
+  Serial.print("ERRORE: ");
+  Serial.println(msg);
+  Serial.print("RESETTARE");
+  while(true){};
+}
 
+void print_status()
+{
+  Serial.println("-----------------------------------------");
+  Serial.println("INGRESSI:");
+  Serial.print("LUCE=");  Serial.print(s.light,DEC);  
+  Serial.print(" ");
+  switch(s.period)  
+  {
+  case day:   Serial.println("GIORNO");  break;
+  case night: Serial.println("NOTTE");   break;
+  default:     error("PERIODO IGNOTO");  break;
+  }
 
+  if ( s.open_btn == PRESSED ){Serial.println("PULSANTE APRI PREMUTO");}else{Serial.println("PULSANTE APRI NON PREMUTO");}
 
+  if ( s.close_btn == PRESSED ){Serial.println("PULSANTE CHIUDI PREMUTO");}else{Serial.println("PULSANTE CHIUDI NON PREMUTO");}
 
+  if ( s.manual_btn == PRESSED ){Serial.println("PULSANTE MANUALE PREMUTO");}else{Serial.println("PULSANTE MANUALE NON PREMUTO");}  
 
+  if ( s.limit_swt == REACHED ){Serial.println("FINECORSA IMPEGNATO");}else{Serial.println("FINECORSA LIBERO");}
 
+  Serial.println("");
+  Serial.println("USCITE:");
+  if ( digitalRead( MOTOR ) == GO ){Serial.println("MOTORE IN MOVIMENTO");}else{Serial.println("MOTORE FERMO");}
 
+  if ( digitalRead( DIRECTION ) == OPEN_DIR ){Serial.println("DIREZIONE APERTURA");}else{Serial.println("DIREZIONE CHIUSURA");}
 
+  switch(s.door)  
+  {
+  case opened: Serial.println("PORTA APERTA"); break;
+  case closed: Serial.println("PORTA CHIUSA"); break;
+  case unknown_status: Serial.println("PORTA ??????"); break;
+  default: error("STATO PORTA INASPETTATO");   break;
+  }
 
-
-
-
-
-
-
-
-
-
+  Serial.println("-----------------------------------------");
+}
 
